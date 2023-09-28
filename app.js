9917c629c4069cdec19ed54715fcd8ba8fc21c4e
ZachPerkins01 <139145292+ZachPerkins01@users.noreply.github.com>
2023-09-28 13:56:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_w5nc78k", "template_5dly9mq", formData)
+    emailjs.send("service_zd74iqg", "template_4txje8n", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
